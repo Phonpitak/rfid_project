d4446c002311481f0601e4e13d88f5348885c895
Phonpitak2 <Phonpitak.work@gmail.com>
2025-01-27 21:02:55 +0700

    boot

--- routes/memberlist.js
+++ routes/memberlist.js
@@ -16,4 +16,30 @@ router.get('/user/memberlist', (req, res) => {
   });
 });
 
+router.get('/member/:id', (req, res) => {
+  const userId = req.params.id;
+  const sql = 'SELECT user_id, std_firstname, std_lastname, b_branch, f_facully FROM t_user WHERE user_id = ?';
+  db.query(sql, [userId], (err, result) => {
+      if (err) {
+          res.status(500).json({ error: 'Database error' });
+      } else {
+          res.json(result[0]); // ส่งข้อมูลผู้ใช้กลับ
+      }
+  });
+});
+
+router.put('/member/update', (req, res) => {
+  const { user_id, std_firstname, std_lastname, b_branch, f_facully } = req.body;
+  const sql = 'UPDATE t_user SET std_firstname = ?, std_lastname = ?, b_branch = ?, f_facully = ? WHERE user_id = ?';
+
+  db.query(sql, [std_firstname, std_lastname, b_branch, f_facully, user_id], (err, result) => {
+      if (err) {
+          res.status(500).json({ error: 'Database error' });
+      } else {
+          res.json({ message: 'Updated successfully' });
+      }
+  });
+});
+
+
 module.exports = router;
